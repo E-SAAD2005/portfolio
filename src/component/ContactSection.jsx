d5b3a9fe4588e3d8e1ed4eaec45ddfd1a2a83800
E-SAAD2005 <saadelaqraa2005@gmail.com>
2025-10-21 08:42:49 +0100

    Replace placeholder links with actual social media URLs
    Updated social media links in ContactSection component.

--- src/component/ContactSection.jsx
+++ src/component/ContactSection.jsx
@@ -93,13 +93,13 @@ import { useState } from "react";
             <div className="pt-8">
               <h4 className="font-medium mb-4"> Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
-                <a href="#" target="_blank">
+                <a href="https://www.linkedin.com/in/saad-el-aqraa-787589305/" target="_blank">
                   <Linkedin />
                 </a>
                 <a href="#" target="_blank">
                   <Twitter />
                 </a>
-                <a href="#" target="_blank">
+                <a href="https://www.instagram.com/lk__saad/" target="_blank">
                   <Instagram />
                 </a>
                 <a href="#" target="_blank">
